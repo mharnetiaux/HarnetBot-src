16e35782a2ebe7c8bfe5654e3ebe245abfb9ec07
mharnetiaux <mattskyh@gmail.com>
2019-03-05 16:23:53 -0800

    messing around with bot service documentation. should prob set up emulator.

--- app.js
+++ app.js
@@ -367,23 +367,13 @@ bot.dialog('GuessAsMine',
 // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        session.send("Welcome to the dinner reservation.");
-        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+        session.send("Welcome to idiom.");
+        builder.Prompts.time(session, "Please provide a troubling phrase and I will provide the wisdom.");
     },
     function (session, results) {
-        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
-        builder.Prompts.text(session, "How many people are in your party?");
-    },
-    function (session, results) {
-        session.dialogData.partySize = results.response;
-        builder.Prompts.text(session, "Whose name will this reservation be under?");
-    },
-    function (session, results) {
-        session.dialogData.reservationName = results.response;
-
-        // Process request and display reservation details
-        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
-        session.endDialog();
+        //session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        //builder.Prompts.text(session, "How many people are in your party?");
+        session.send(results.response);
     }
 ]).set('storage', tableStorage); // Register in-memory storage
 
