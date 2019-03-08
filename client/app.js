0f02c637b6fd0ef0fed124a5958d3733e15361c5
mharnetiaux <mattskyh@gmail.com>
2019-03-08 11:31:25 -0800

    update to webchat form.

--- client/app.js
+++ client/app.js
@@ -2,22 +2,40 @@ import { DirectLine } from 'botframework-directlinejs';
 import './styles/app.styles.less';
 import React, {Component} from 'react';
 import ReactWebChat from 'botframework-webchat';
+import axios from 'axios';
 import {render} from 'react-dom';
 
 class App extends Component{
     constructor(props){
         super(props);
         this.state = {
-            token: 'OZZC4b2L4Yo.dAA.MgBEAFIAbwAyAFkAegBGADMATQBQAEUAYQBDAFcAYwBFADkAdgBNAFYAZgAtAGcA.IkahKsbU1AE.xFcF3xA0QBs.gG4T5xMyC3GPDUrwt28TIoOKQJQ5B52SJ5Expq2s4Hs',
-            user: 'C0AAYOXoBIY4FALzDgrIyo-g'
+            directLine: undefined
         };
-        this.directLine = new DirectLine({ token: this.state.token});
+    }
+
+    componentDidMount() {
+        axios({
+            method: 'post',
+            url: 'https://directline.botframework.com/v3/directline/tokens/generate',
+            headers: {
+                Accept: "*/*",
+                Authorization: "Bearer OZZC4b2L4Yo.YTksjo_Z0IU0bu8QgCeDHgVibWcEk4ooaZgyiD4vvHE",
+            }
+        }).then(response => {
+            this.setState({
+                directLine: new DirectLine({ token: response.data.token})
+            });
+        });
     }
 
     render() {
         return (
             <section>
-                <ReactWebChat directLine={ this.directLine } userID={this.state.user}/>
+                <h2>Welcome to Idiom</h2>
+                <p>Search for an idiom and I will provide the meaning.</p>
+                {
+                    this.state.directLine ? <ReactWebChat id="test" className="webChat" directLine={this.state.directLine}/> : <div>Good Bye</div>
+                }
             </section>
         )
     }
