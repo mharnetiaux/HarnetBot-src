ade79761f69427b0bdd10309b9a085424698f458
mharnetiaux <mattskyh@gmail.com>
2019-03-11 10:53:19 -0700

    now server should render dist build instead of dev.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import exphbs  from 'express-handlebars';
 import webpack from 'webpack';
+import env from './webpack/environment';
 import webpack_dev_middleware from 'webpack-dev-middleware';
 import webpack_hot_middleware from 'webpack-hot-middleware';
 import webpackConfig from './webpack.config.babel';
@@ -24,8 +25,13 @@ app.use(express.static(webpackConfig.output.path), middleware);
 
 app.use(hot_middleware);
 
+console.log(process.env);
+
 app.get('/',(req, res) => {
-    res.render('home');
+    if(env.isDev) {
+        res.render('home');
+    }
+    res.render('./dist/index.html')
 });
 
 app.listen(port);
